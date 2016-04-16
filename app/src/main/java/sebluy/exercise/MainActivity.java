f2cfeef60a7a5b10f8b92e3c626635807a97eb6e
Sebastian Luy <sebastian.a.luy@gmail.com>
2016-04-16 10:25:33 -0400

    Initial commit, back to java

---
+++ app/src/main/java/sebluy/exercise/MainActivity.java
@@ -0,0 +1,35 @@
+package sebluy.exercise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+
+import trikita.anvil.RenderableView;
+
+import static trikita.anvil.DSL.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String[] workoutNames = {"Static Core", "Calisthenic"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new RenderableView(this) {
+            @Override
+            public void view() {
+               linearLayout(() -> {
+                   size(MATCH, MATCH);
+                   orientation(LinearLayout.VERTICAL);
+
+                   listView(() -> {
+                       size(FILL, WRAP);
+                       adapter(new ArrayAdapter<>(
+                            MainActivity.this, android.R.layout.simple_list_item_1, workoutNames)) ;
+                   });
+               });
+            }
+        });
+    }
+}
